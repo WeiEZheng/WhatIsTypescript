0a66d1edddd35845adc789727f67f58cb6065399
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-04-09 19:15:57 -0400

    added dictionaryApi to grab data from api

--- dictionary/src/App.tsx
+++ dictionary/src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [definition, setDefintion] = useState([]);
+  const dictionaryApi =async (word:String) => {
+    try {
+      const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+      setDefintion(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const form: HTMLFormElement = document.querySelector('#defineform')!;
+
+  useEffect(()=>{
+    form.onsubmit = () => {
+      const formData = new FormData(form);
+      const text = formData.get('defineword') as string;
+      dictionaryApi(text);
+    };
+  },[])
+  
+  // const form: HTMLFormElement = document.querySelector('#defineform');
+
+  // form.onsubmit = () => {
+  //   const formData = new FormData(form);
+
+  //   console.log(formData);
+  //   const text = formData.get('defineword') as string;
+  //   console.log(text);
+  //   return false; // prevent reload
+  // }
   return (
     <div className="App">
       <header className="App-header">
