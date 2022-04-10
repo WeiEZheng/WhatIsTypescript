6bb2973c3ed40ded43085745d11f66cd52f08df7
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-04-10 01:10:58 -0400

    display header

--- dictionary/src/App.tsx
+++ dictionary/src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { text } from 'stream/consumers';
 
 function App() {
+  const [word, setWord] = useState("");
   const [definition, setDefintion] = useState([]);
-  const dictionaryApi =async (word:String) => {
+
+  const dictionaryApi = async (word: string) => {
     try {
-      const data = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/" + word);
+      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word);
       setDefintion(data.data);
     } catch (error) {
       console.log(error);
@@ -15,41 +18,24 @@ function App() {
 
   const form: HTMLFormElement = document.querySelector('#defineform')!;
 
+  form.onsubmit = () => {
+    const formData = new FormData(form);
+    const text = formData.get('defineword') as string;
+    setWord(text);
+    return false;
+  }
   useEffect(()=>{
-    form.onsubmit = () => {
-      const formData = new FormData(form);
-      const text = formData.get('defineword') as string;
-      dictionaryApi(text);
-    };
-  },[])
+      dictionaryApi(word);
+  },[word])
+
+  console.log(definition);
   
-  // const form: HTMLFormElement = document.querySelector('#defineform');
-
-  // form.onsubmit = () => {
-  //   const formData = new FormData(form);
-
-  //   console.log(formData);
-  //   const text = formData.get('defineword') as string;
-  //   console.log(text);
-  //   return false; // prevent reload
-  // }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (      
+  <div className="App">
+    <header>{word}</header>
+    <li>{}</li>
+  </div> 
+);
 }
 
 export default App;
